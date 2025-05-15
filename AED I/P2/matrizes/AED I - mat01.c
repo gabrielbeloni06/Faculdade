9ecc19026e417f614aabedd151a63f8beae80969
Gabriel Beloni <gabrielbelonibr@gmail.com>
2025-05-15 15:12:31 -0300

    AED-matriz01

---
+++ AED I/P2/matrizes/AED I - mat01.c
@@ -0,0 +1,18 @@
+/*Apresentação de Matriz estática*/
+#include <stdio.h>
+int main(){
+    int n,m,mat[3][3];
+    mat[3][3]={1,2,3,4,5,6,7,8,9};
+    /* | c0  | c1  | c2  |
+    l0 |  1  |  2  |  3  |
+    l1 |  4  |  5  |  6  |
+    l2 |  7  |  8  |  9  |
+    */
+   for(int i=0;i<3;i++){
+        for(int j=0;j<3;j++){
+            printf("%d ", mat[i][j]);
+        }
+        printf("\n");
+   }
+   return 0;
+}
