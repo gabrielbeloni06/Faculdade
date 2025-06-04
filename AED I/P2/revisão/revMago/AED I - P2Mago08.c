6b88b9a891018e2c301c7e87450b7609ecced44e
Gabriel Beloni <gabrielbelonibr@gmail.com>
2025-06-04 19:17:52 -0300

    RevisãoP2-08

---
+++ AED I/P2/revisão/revMago/AED I - P2Mago08.c
@@ -0,0 +1,6 @@
+/*Função para calcular a determinante de uma matriz lxc, sendo l<=3 e c<=3*/
+double deter(double mat[3][3]){
+    return mat[0][0] * (mat[1][1] * mat[2][2] - mat[1][2] * mat[2][1])
+         - mat[0][1] * (mat[1][0] * mat[2][2] - mat[1][2] * mat[2][0])
+         + mat[0][2] * (mat[1][0] * mat[2][1] - mat[1][1] * mat[2][0]);
+}
