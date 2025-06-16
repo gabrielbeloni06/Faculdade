dd494d31f8064a7fda216b1af4c5d462628075b8
Gabriel Beloni <gabrielbelonibr@gmail.com>
2025-06-16 16:30:26 -0300

    LIP:Construtores/Destrutores - Ex02 C++

---
+++ 1º Periodo/LIP/C++/ContrutoresDestrutores/Ex02.cpp
@@ -0,0 +1,47 @@
+/*Ex02 - Lista02 - Gabriel Egídio Santos Beloni*/
+#include <iostream>
+#include <string>
+#include <iomanip>
+#include <algorithm>
+#include <limits>
+using namespace std;
+class Produto{
+    private:
+        string nome;
+        float preco;
+    public:
+        Produto(string n, float p){
+            nome=n;
+            preco=p;
+        }
+        Produto(string n){
+            nome=n;
+            preco=0.0f;
+        }
+        void imprimir(){
+            cout << fixed << setprecision(2);
+            cout << "Produto criado: " << nome << endl;
+            cout << "Preco: " << preco <<endl;
+        }
+        void setPreco(float p2){
+            preco=p2;
+        }
+        ~Produto(){
+            cout << "Produto Destruido: " << nome << endl;
+        }
+};
+int main(){
+    string n,n2;
+    float p,p2;
+    getline(cin,n);
+    cin>>p;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    getline(cin,n2);
+    cin>>p2;
+    Produto a(n,p);
+    Produto b(n2);
+    b.setPreco(p2);
+    a.imprimir();
+    b.imprimir();
+    return 0;
+}
