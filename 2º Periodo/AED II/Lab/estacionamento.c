ac4d3abf4fed235ee66c7a5cc1d50212d6821934
gabriel <gabrielbelonibr@gmail.com>
2025-10-28 08:49:10 -0300

    Ex - P2 AEDs II

---
+++ 2º Periodo/AED II/Lab/estacionamento.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdbool.h>
+int calculo(){
+	int k,n,i;
+	scanf("%d %d",&k, &n);
+	int *C[n], *S[n], *v1[k], *v2[k];
+	while(i<n){
+		scanf("%d", & *(C[i]));
+		scanf("%d", & *(S[i]));
+		i++;
+	}
+	while(k){
+                v[i] = C[i];
+       		v2[i] = S[i];
+	}
+	bool resp = false;
+	if(v2[k - 1] < C[n - 1]) resp = true;
+}
+int main(){
+	calculo();
+	return 0;
+}
